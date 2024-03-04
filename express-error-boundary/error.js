38abc3c815cbb35fdeee130bcdca94c3afb9d966
M. Feyza Yıldırım <144602340+Mfeyza@users.noreply.github.com>
2024-03-04 14:00:05 +0300

    status-

---
+++ express-error-boundary/error.js
@@ -0,0 +1,63 @@
+"use strict";
+
+/* -------------------------------------------------------------------------- */
+/*                          EXPRESS-ERROR MANAGEMENT                          */
+/* -------------------------------------------------------------------------- */
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+const PORT = process.env.PORT || 8000;
+/* -------------------------------------------------------------------------- */
+// app.get("/", (req, res) => {
+//   // res.send('hello')
+//   // console.log(abc)
+//   throw new Error("error message");
+// });
+
+// app.get("/user/:id", (req, res) => {
+//   const id = req.params?.id || 0;
+//   try {
+//     if (isNaN(id)) {
+//       throw new Error("ID is not a number", { cause: "params.id =" + id });
+//     } else {
+//       res.send({
+//         message: "OK",
+//         id,
+//       });
+//     }
+//   } catch (err) {
+//     res.send({
+//         error:true,
+//         message: err.message
+//     })
+//   }
+// });
+
+
+/* -------------------------------------------------------------------------- */
+/*                                ERROR HANDLER                               */
+/* -------------------------------------------------------------------------- */
+
+app.get("/*", (req, res) => {
+    res.errorStatusCode=404
+    throw new Error("error message");
+  });
+/* -------------------------------------------------------------------------- */
+/* -------------------------------------------------------------------------- */
+/* -------------------------------------------------------------------------- */
+
+
+  const errorHandler= (err,req,res,next)=>{
+    console.log('error handle started')
+    const errorStatusCode=res?.errorStatusCode || 500
+    res.status(errorStatusCode).send({
+        error:true,
+        message:err.message 
+   })
+  }
+  app.use(errorHandler)
+/* -------------------------------------------------------------------------- */
+
+
+app.listen(PORT, () => console.log("Running : http://127.0.0.1:" + PORT));
